465bec8b6846f3d813be42e9791910f6161df38e
Abhishek333k <59314939+Abhishek333k@users.noreply.github.com>
2026-01-30 16:40:51 +0530

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -20,7 +20,7 @@ const CONTACT_INFO = {
     
     // Maps Links
     map_link: "https://maps.app.goo.gl/VGqQCjRQCx729Qmd6", 
-    map_embed_url: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d359.0849250400801!2d80.5657855623069!3d16.42902986953823!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a35f1606165d7b7%3A0xc31ff66a47bd70ce!2sSAGAR%20BOREWELLS!5e1!3m2!1sen!2sin!4v1769771062388!5m2!1sen!2sin",
+    map_embed_url: "https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d3826.900591410121!2d80.56646468591508!3d16.429874347556925!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a35f1606165d7b7%3A0xc31ff66a47bd70ce!2sSAGAR%20BOREWELLS!5e0!3m2!1sen!2sin!4v1769771375305!5m2!1sen!2sin",
 
     // Socials
     social_instagram: "https://www.instagram.com/sagar_bore_wells/",
