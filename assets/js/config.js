986c97026769e5bc46c0f8828c4383a3657c7872
Abhishek333k <59314939+Abhishek333k@users.noreply.github.com>
2026-01-30 13:57:11 +0530

    Fix comma placement in CONTACT_INFO object
    Fixed the comma placement in the google_maps_key entry.

--- assets/js/config.js
+++ assets/js/config.js
@@ -1,26 +1,14 @@
 // FILENAME: assets/js/config.js
 
 const CONTACT_INFO = {
-    // The Master Phone Number
     whatsapp_api: "916304094177", 
-    
-    // Display format for the screen
     phone_display: "+91 63040-94177", 
-    
-    // Support Email
     email: "support@sagarborewells.com",
-    
-    // Office Address
     address: "Mangalagiri, Andhra Pradesh",
-    
-    // Google Maps API Key
-    google_maps_key: "AIzaSyDkHaU8FfYd2vQWHiU02yjA_7DrsOWHYus", // <--- I ADDED THE COMMA HERE
-
-    // Your Database URL
+    google_maps_key: "AIzaSyDkHaU8FfYd2vQWHiU02yjA_7DrsOWHYus", // Comma Fixed
     database_url: "https://script.google.com/macros/s/AKfycbwMJ16yDE-PsghDqyBa6mS4J-QXrMn10OYSEthKZEMRhv9uw6N1NpBN3_FgNX7PsmeSig/exec"
 };
 
-// Function to inject details into HTML
 function loadContactDetails() {
     const phoneElements = document.querySelectorAll('.dynamic-phone');
     phoneElements.forEach(el => {
